29298e606bf2adf6c7374553a4f794f18a5cac8d
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-04-06 22:23:00 +0200

    add parser
    1. import Mparser.py from example
    2. add parser to main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sys
 from scanner import lexer, find_column
+from Mparser import parser
 
 if __name__ == '__main__':
-    path = sys.argv[1] if len(sys.argv) > 1 else 'example_full.txt'
+    path = sys.argv[1] if len(sys.argv) > 1 else 'example1.m'
     with open(path, 'r') as f:
         text = f.read()
 
-    lexer.input(text)
-    for token in lexer:
-        # column = find_column(text, token)
-        # print('(%d,%d): %s(%s)' % (token.lineno, column, token.type, token.value))
-        print('(%d): %s(%s)' % (token.lineno, token.type, token.value))
+    # lexer.input(text)
+    # for token in lexer:
+    #     # column = find_column(text, token)
+    #     # print('(%d,%d): %s(%s)' % (token.lineno, column, token.type, token.value))
+    #     print('(%d): %s(%s)' % (token.lineno, token.type, token.value))
+
+    parser.parse(text, lexer=lexer)
