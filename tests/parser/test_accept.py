45c664eded0c1cba3702eb5ea683b74120537c44
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-04-09 22:30:40 +0200

    accept examples test

---
+++ tests/parser/test_accept.py
@@ -0,0 +1,15 @@
+import pytest
+from pathlib import Path
+from Mparser import parser
+
+
+@pytest.mark.parametrize('name', (
+    'example1.m',
+    'example2.m',
+    'example3.m',
+))
+def test_accept(name):
+    input_path = Path(__file__).parent / 'resources' / name
+    with open(input_path, 'r') as f:
+        text = f.read()
+    parser.parse(text)
