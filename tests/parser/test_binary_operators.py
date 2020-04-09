f222a0b3952c5a90972e8ab8a8d93adec2a9c729
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-04-09 22:56:25 +0200

    binary operators tests

---
+++ tests/parser/test_binary_operators.py
@@ -0,0 +1,77 @@
+import pytest
+from itertools import chain, product
+from Mparser import parser
+from ast_ import *
+
+
+@pytest.mark.parametrize('add_op', ('+', '-', '.+', '.-'))
+@pytest.mark.parametrize('mul_op', ('*', '/', '.*', './'))
+def test_mul_op_over_add_op_precedence(add_op, mul_op):
+    text = f"foo = 123 {add_op} 456 {mul_op} 789;"
+    ast = parser.parse(text)
+    assert ast == Program(
+        Instructions([
+            Assignment(
+                '=',
+                Variable('foo'),
+                BinExpr(
+                    add_op,
+                    IntNum(123),
+                    BinExpr(
+                        mul_op,
+                        IntNum(456),
+                        IntNum(789)
+                    )
+                )
+            )
+        ])
+    )
+
+
+@pytest.mark.parametrize('op1, op2', chain(
+    product(('+', '-', '.+', '.-'), repeat=2),
+    product(('*', '/', '.*', './'), repeat=2),
+))
+def test_left_associativity(op1, op2):
+    text = f"foo = 123 {op1} 456 {op2} 789;"
+    ast = parser.parse(text)
+    assert ast == Program(
+        Instructions([
+            Assignment(
+                '=',
+                Variable('foo'),
+                BinExpr(
+                    op2,
+                    BinExpr(
+                        op1,
+                        IntNum(123),
+                        IntNum(456)
+                    ),
+                    IntNum(789)
+                )
+            )
+        ])
+    )
+
+
+@pytest.mark.parametrize('op1, op2', product(('+', '-', '*', '/', '.+', '.-', '.*', './'), repeat=2))
+def test_expression_group(op1, op2):
+    text = f"foo = (123 {op1} 456) {op2} 789;"
+    ast = parser.parse(text)
+    assert ast == Program(
+        Instructions([
+            Assignment(
+                '=',
+                Variable('foo'),
+                BinExpr(
+                    op2,
+                    BinExpr(
+                        op1,
+                        IntNum(123),
+                        IntNum(456)
+                    ),
+                    IntNum(789)
+                )
+            )
+        ])
+    )
