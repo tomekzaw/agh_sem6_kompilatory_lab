688c40e48e663292b57b623329fd8bebb50a8964
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2020-04-12 14:09:10 +0200

    for loop tests

---
+++ tests/parser/test_for.py
@@ -0,0 +1,114 @@
+import pytest
+from Mparser import parser
+from ast_ import *
+
+
+def test_for_single_instruction():
+    text = """
+    for i = 1:10
+        print i;
+    """
+    ast = parser.parse(text)
+    assert ast == Program(
+        Instructions([
+            For(
+                Variable('i'),
+                Range(
+                    IntNum(1),
+                    IntNum(10),
+                ),
+                Print([
+                    Variable('i')
+                ])
+            )
+        ])
+    )
+
+def test_for_instruction_block():
+    text = """
+    for i = 1:10 {
+        print i;
+    }
+    """
+    ast = parser.parse(text)
+    assert ast == Program(
+        Instructions([
+            For(
+                Variable('i'),
+                Range(
+                    IntNum(1),
+                    IntNum(10),
+                ),
+                Instructions([
+                    Print([
+                        Variable('i')
+                    ])
+                ])
+            )
+        ])
+    )
+
+
+def test_nested_for():
+    text = """
+    for i = 1:10
+        for j = 1:10
+            print i, j;
+    """
+    ast = parser.parse(text)
+    assert ast == Program(
+        Instructions([
+            For(
+                Variable('i'),
+                Range(
+                    IntNum(1),
+                    IntNum(10),
+                ),
+                For(
+                    Variable('j'),
+                    Range(
+                        IntNum(1),
+                        IntNum(10)
+                    ),
+                    Print([
+                        Variable('i'),
+                        Variable('j')
+                    ])
+                )
+            )
+        ])
+    )
+
+
+def test_for_range_expression():
+    text = """
+    for i = 2+3*5 : n*n
+        print i;
+    """
+    ast = parser.parse(text)
+    assert ast == Program(
+        Instructions([
+            For(
+                Variable('i'),
+                Range(
+                    BinExpr(
+                        '+',
+                        IntNum(2),
+                        BinExpr(
+                            '*',
+                            IntNum(3),
+                            IntNum(5)
+                        )
+                    ),
+                    BinExpr(
+                        '*',
+                        Variable('n'),
+                        Variable('n')
+                    )
+                ),
+                Print([
+                    Variable('i')
+                ])
+            )
+        ])
+    )
